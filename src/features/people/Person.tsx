fa9807b1a829f9468ca5cd7bc974504c694a7135
Amir <iknubs@gmail.com>
2022-06-22 12:41:34 -0700

    Show/Edit person page implementation

--- src/features/people/Person.tsx
+++ src/features/people/Person.tsx
@@ -1,5 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import ButtonGroup from './ButtonGroup';
 
-export default function Person() {
-  return <div>Person</div>;
+export default function Person(props:any) {
+  const [firstName, setFirstName] = useState(props.person.firstName);
+  const [lastName, setLastName] = useState(props.person.lastName);
+  const [email, setEmail] = useState(props.person.email);
+  const [isEditing, setIsEditing] = useState(props.personToEdit === props.person.id);
+  useEffect(() => {
+      setIsEditing(props.personToEdit === props.person.id);
+  }, [props.personToEdit, props.person.id])
+
+  function submitHandler(e:any) {
+      e.preventDefault();
+      const formData = {
+          post: {
+              id: props.person.id,
+              firstName: firstName,
+              lastName: lastName,
+              email: email
+          }
+      }
+      props.submitEdit(formData)
+      resetState();
+  }
+
+  function resetState() {
+    setFirstName(props.person.firstName);
+    setLastName(props.person.lastName);
+    setEmail(props.person.email);
+  }
+
+  const firstNameElement = <h2 className="title text-start">{props.person.firstName}</h2>;
+  const lastNameElement = <h2 className="title text-start">{props.person.lastName}</h2>;
+  const emailElement = <h2 className="title text-start">{props.person.email}</h2>;
+  const editableFirstName = <input 
+                          type="text" 
+                          className="form-control text-start" 
+                          value={firstName} 
+                          onChange={(e) => setFirstName(props.person.firstName)} />;
+  const editableLastName = <input 
+                          type="text" 
+                          className="form-control text-start" 
+                          value={lastName} 
+                          onChange={(e) => setLastName(props.person.lastName)} />;
+  const editableEmail = <input 
+                          type="text" 
+                          className="form-control text-start" 
+                          value={email} 
+                          onChange={(e) => setEmail(props.person.email)} />;
+  const submitButton = <button
+                          type="submit"
+                          className="form-control"
+                          onClick={(e) => submitHandler(e)}>Submit</button>;
+
+  const displayUserDetails = <div>
+      {firstNameElement}
+      {lastNameElement}
+  </div>
+
+const editUserDetails = <div>
+      {editableFirstName}
+      {editableLastName}
+  </div>
+
+  
+return <div>
+    <div className="row">
+        <div className="col-8">
+            {isEditing ? editUserDetails : displayUserDetails}
+        </div>
+        <div className="col-4">
+            <ButtonGroup 
+              person_id={props.person.id}
+              dispatch={props.dispatch}
+              toggleEditForm={props.toggleEditForm}
+              />
+
+        </div>
+    </div>
+      <div className="row">
+          <div className="col-8">
+              {isEditing ? editableEmail : emailElement}
+          </div>
+      </div>
+      <div className="row">
+          <div className="col-2">
+              {isEditing ? submitButton : ""}
+          </div>
+      </div>
+</div>;
 }
